a6ba901ed1e508a5758e80ad70b9f837f76b6bc3
Mayard89 <96402345+Mayard89@users.noreply.github.com>
2021-12-20 10:25:44 +0700

    Create VRSC.sh

---
+++ VRSC.sh
@@ -0,0 +1,6 @@
+# VRSC
+sudo apt update
+sudo apt upgrade
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3960#xnsub -u RVYSkqq81n4WRFPXVVR43M3Quv4bL4uLEP.githubt -p x --cpu 16
